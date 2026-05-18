83e4f75a1d6dac18d2d3c8448adfe53770addb5b
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2026-05-17 18:41:23 -0700

    Implement sum

--- src/Exercises.java
+++ src/Exercises.java
@@ -12,7 +12,19 @@ public class Exercises {
      * @return sum of the values in the list
      */
     public static int sum(ListNode head) {
-        return -1;
+        if (head == null) {
+            return 0;
+        }
+
+        int total = 0;
+        ListNode current = head;
+
+        while (current != null){
+            total += current.data;
+            current = current.next;
+        }
+        
+        return total;
     }
 
     /**
